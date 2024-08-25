9ecb765a63a806d43bb0ea8eb259110326dfde2b
James Baranuelo <122170760+Pendragon05@users.noreply.github.com>
2024-08-26 08:25:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+/* Customer Counter App */
+
 let count = 0
 
+const countEl = document.getElementById("count-el");
+
 function increment() {
-    count += 1
-    countEl.textContent = count
+    count += 1;
+    countEl.textContent = count;
+};
+
+function decrease() {
+  if (count != 0) {
+    count -= 1;
+    countEl.textContent = count;
+
+  }
+
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+function reset() {
+  count = 0;
+  countEl.textContent = count;
 }
